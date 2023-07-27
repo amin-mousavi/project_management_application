37433a8b1ebd82e3ecd7ffb11bc85bacbac5badd
Amin Mousavi <seyedamin963@gmail.com>
2023-07-27 17:06:19 +0330

    Enhance admin panel and coustomize it for our project management app.

--- projects/admin.py
+++ projects/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from.models import Project,Task
 
 
-admin.site.register(Project)
-admin.site.register(Task)
+class TaskInline(admin.StackedInline):
+    model = Task
+    extra = 2
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [TaskInline, ]
+    list_display = ['name', 'date_created']
+    search_fields = ['name', 'description']
+    list_filter = ['date_created',]
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'assignee', 'project', 'date_created', 'due_date', 'status']
+    search_fields = ['title', 'assignee', 'project', 'status', 'description']
+    list_filter = ['assignee', 'project', 'status', 'date_created', 'due_date']
+    
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
