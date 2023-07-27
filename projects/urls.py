ff46b4a2945335743255522422b1d230028338a5
Amin Mousavi <seyedamin963@gmail.com>
2023-07-27 18:02:00 +0330

    Create a page for list of all project and create one page for each projct to show detail of project and show tasks.

---
+++ projects/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('home', views.home, name = 'home'),
+    path('projects/', views.projectList, name = 'projects'),
+    path('projects/<int:pk>', views.projectDetail, name ='project-detail'), 
+
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Project
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def projectList(request):
+   projects = Project.objects.all()
+   
+   context = {'projects':projects}
+   return render(request, 'projects/projects.html',context)
+
+
+
+def projectDetail(request,pk):
+   project = get_object_or_404(Project, id=pk)
+   project_tasks = project.task_set.all()
+   
+   context = {'project':project,'project_tasks':project_tasks}
+   return render(request, 'projects/project-detail.html',context)
